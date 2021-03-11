b2ed5e5ef50b6cbc5f43f738e5b46f1c48698967
Sarah Obasi <sarahrichie08@gmail.com>
2021-03-11 12:40:44 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "cca255cfe75e7ee795ea53b61bead1bb"
+    }
+}
+
+module.exports = constants;
